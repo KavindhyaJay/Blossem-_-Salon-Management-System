a57c84b7866eb868e52eb95438ef7e9ac6c90b17
Rivish <rivish2022@gmail.com>
2025-12-26 20:36:52 +0530

    commiting backend full

--- salonmanagement/Salonms/Salonms/src/main/java/com/fullstack/Salonms/repository/StaffRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fullstack.Salonms.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.fullstack.Salonms.model.Staff;
-
-public interface StaffRepository extends MongoRepository<Staff, String> {
-
-}
